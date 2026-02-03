8c580279176db81bafbe1dd67f97ebe89d33eaa8
SystemInfomation <blakeflyz1@gmail.com>
2026-02-03 18:39:21 -0500

    fa

--- config/gamePaths.js
+++ config/gamePaths.js
@@ -1,12 +1,6 @@
 // Map of game URLs to their local paths in the public directory
-const gamePaths = {
-  'madalin-stunt-cars-2': '/games/madalin-stunt-cars-2/index.html',
-  'ducklife1': '/games/ducklife4/index.html', // Assuming ducklife1 uses ducklife4 files
-  'ducklife2': '/games/ducklife5/index.html', // Assuming ducklife2 uses ducklife5 files
-  'ducklife3': '/games/ducklife4/index.html', // Update this if you have ducklife3 specific files
-  'ducklife4': '/games/ducklife4/index.html',
-  'geodash': '/games/geodash/index.html',
-  'polytrack': '/games/polytrack/index.html'
-};
+// Since all games are now served from the Celestrium GitHub repository,
+// this mapping is no longer needed as the proxy system handles external URLs
+const gamePaths = {};
 
 export default gamePaths;
